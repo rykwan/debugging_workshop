6e6c5e9ba2daa5e4c3ba9194ad47dfa3cb95faf6
rykwan <raykwan@ucla.edu>
2020-04-16 16:15:13 -0700

    add solution to LL example

--- SOLUTIONscoretrackerLL.cpp
+++ SOLUTIONscoretrackerLL.cpp
@@ -14,7 +14,7 @@ struct Node {
 };
 
 void printLL(Node* head) {
-    Node* p;
+    Node* p = head; /// FIXED SEGFAULT. initialize with head
     while (p != NULL) {
         cout << p->val;
         if (p->next != NULL)
@@ -30,6 +30,7 @@ void insertAtFront(Node* &head, int x) {
     
     Node* newNode = new Node(x);
     newNode->next = head;
+    head = newNode; // FIXED, we need head to point to the new head node
 }
 
 class ScoreTracker {
@@ -38,10 +39,11 @@ class ScoreTracker {
   
   void addScore(int score) {
     insertAtFront(scoresListHead, score);
+    ++nScores_; // FIXED, increment nScores_
   }
 
   void printStats() {
-    if (nScores_ > 0) {
+    if (nScores_ <= 0) { // FIXED > to <=
       cout << "No scores yet.\n";
       return;
     }
@@ -57,7 +59,7 @@ class ScoreTracker {
       }
       p = p->next;
     }
-    double pctAboveAvg = (nAboveAvg / nScores_) * 100;
+    double pctAboveAvg = (nAboveAvg * 100.0) / nScores_; // FIXED: must do double division
     cout << pctAboveAvg << "% scored above average." << endl;
   }
 
